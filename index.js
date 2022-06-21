5d5caf1d6a411c9e19cc2f86c6fdf2e3e64f18e5
Zakaria Ali <najkacade100@gmail.com>
2022-06-21 15:14:04 +0300

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const input = document.getElementById('input');
-
 function addingEventListener() {
-  input.addEventListener('click', function() {
-    alert('I was clicked!');
-  });
+  const input = document.getElementById('input');
+  input.addEventListener('click', clickAlert());
+  function clickAlert() {
+      alert('I was clicked!');
+    }
+
+
 }
-input.addEventListener('click', clickAlert);
+addingEventListener()
